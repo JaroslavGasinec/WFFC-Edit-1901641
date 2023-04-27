c6d71f2d9ff7d06b67d1042ce42921b58a089e88
Jaroslav Gasinec <1901641@gmail.com>
2023-04-27 18:36:58 +0100

    partial implementation of camera

--- WOFFCEdit/Camera.cpp
+++ WOFFCEdit/Camera.cpp
@@ -1,6 +1,75 @@
 #include "Camera.h"
+#include "SceneObject.h"
 
-void Camera::Rotate(const Vector3& angle) 
+Camera::Camera()
 {
-	m_camOrientation += angle;
+	m_camPosition = Vector3(0, 0, 0);
+	m_camOrientation = Rotator(0, 0, 0);
+	m_camLookAt = Vector3(1, 0, 0);
+	m_camLookDirection = Vector3(1, 0, 0);
+	m_camRight = Vector3(0, 0, 0);
+	m_camUp = Vector3(0, 0, 0);
+	m_moveSpeed = 5;
+	m_camRotRate = 10;
+}
+
+void Camera::Rotate(const Rotator& offsetAngle)
+{
+	m_camOrientation += offsetAngle;
+}
+
+void Camera::Move(const Vector3& offset, const bool relative)
+{
+	if (relative)
+	{
+		Vector3 final;
+		final = offset.x * m_camLookDirection;
+		final += offset.z * m_camRight;
+		final += offset.y * m_camUp;
+		m_camPosition += final * m_moveSpeed;
+		return;
+	}
+
+	m_camPosition += offset * m_moveSpeed;
+}
+
+void Camera::CalculateLookAtVector() 
+{	
+	if (!m_focusOject)
+	{
+		const float yaw = m_camOrientation.Yaw();
+		const float pitch = m_camOrientation.Pitch();
+
+		m_camLookDirection.x = cos(yaw) * cos(pitch);
+		m_camLookDirection.z = sin(yaw) * cos(pitch);
+		m_camLookDirection.y = sin(pitch);
+		m_camLookDirection.Normalize();
+		return;
+	}
+
+	m_camLookDirection.x = m_focusOject->posX - m_camPosition.x;
+	m_camLookDirection.y = m_focusOject->posX - m_camPosition.y;
+	m_camLookDirection.z = m_focusOject->posX - m_camPosition.z;
+	m_camLookDirection.Normalize();
+}
+
+void Camera::CalculateRightVector()
+{
+	const float roll = m_camOrientation.Roll();
+	const float yaw = m_camOrientation.Yaw();
+	const float pitch = m_camOrientation.Pitch();
+
+	if (!m_focusOject)
+	{
+		m_camRight.x = (-cos(yaw) * sin(pitch) * sin(roll)) - (sin(yaw) * cos(roll));
+		m_camRight.z = (-sin(yaw) * sin(pitch) * sin(roll)) + (cos(yaw) * cos(roll));
+		m_camRight.y = cos(pitch) * sin(roll);
+		m_camRight *= -1;
+		m_camRight.Normalize();
+	}
+}
+
+void Camera::CalculateUpVector()
+{
+	m_camRight.Cross(m_camLookDirection, m_camUp);
 }
--- WOFFCEdit/Camera.h
+++ WOFFCEdit/Camera.h
@@ -8,32 +8,76 @@ typedef DirectX::SimpleMath::Matrix Matrix;
 // forward declarations
 class SceneObject;
 
+struct Rotator 
+{
+	Rotator() {};
+	Rotator(const float Roll, const float Pitch, const float Yaw) 
+	{
+		this->Roll() = Roll;
+		this->Pitch() = Pitch;
+		this->Yaw() = Yaw;
+	}
+	
+	// x = Roll, Y = Yaw, Z = Pitch
+	Vector3 data;
+
+	float& Roll() { return data.x; }
+	float& Pitch() { return data.z; }
+	float& Yaw() { return data.y; }
+
+	Rotator operator+ (const Rotator& rhs) 
+	{
+		Rotator out;
+		out.data = data + rhs.data;
+		return out;
+	}
+
+	Rotator operator- (const Rotator& rhs)
+	{
+		Rotator out;
+		out.data = data - rhs.data;
+		return out;
+	}
+
+	void operator+= (const Rotator& rhs)
+	{
+		data += rhs.data;
+	}
+
+	void operator-= (const Rotator& rhs)
+	{
+		data -= rhs.data;
+	}
+};
+
 class Camera
 {
 public:
 	Camera();
 	~Camera();
 
-	void Rotate(const Vector3& angle);
+	void Rotate(const Rotator& offsetAngle);
 	void Move(const Vector3& offset, const bool relative = false);
 	void SetFocus(std::shared_ptr<SceneObject> focus);
 	void UnsetFocus();
+	void Update();
 
 	Matrix GetLookAtMatrix();
 
 private:
 
 	Vector3 m_camPosition;
-	Vector3 m_camOrientation;
+	Rotator m_camOrientation;
 	Vector3 m_camLookAt;
 	Vector3 m_camLookDirection;
 	Vector3 m_camRight;
 	Vector3 m_camUp;
 	float m_moveSpeed;
 	float m_camRotRate;
-	bool m_arcMode;
 
 	std::shared_ptr<SceneObject> m_focusOject;
 
 	void CalculateLookAtVector();
+	void CalculateRightVector();
+	void CalculateUpVector();
 };
